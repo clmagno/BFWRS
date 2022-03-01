c2ce3ddf7957fcd42a8339f1bb482d9e46152019
= <=chris.ultd@gmail.com>
2022-03-01 23:29:39 +0800

    initial upload

---
+++ sql/db.php
@@ -0,0 +1,16 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$db_name = "bfwrs";
+
+$conn = mysqli_connect($servername, $username, $password, $db_name);
+
+if (!$conn) {
+    echo "Connection failed!";
+    die("Could not connect to MySQL server.");
+}
+else{
+//   echo "hello";
+}
